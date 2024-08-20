efed28cd1062fd3154e8bfe9778d7d43c1c01427
JoelLucaAdams <joelucadams@gmail.com>
2024-08-20 20:39:08 +0100

    Initial Commit

---
+++ src/main/java/com/joelinrome/PlasmaCraftClient.java
@@ -0,0 +1,10 @@
+package com.joelinrome;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PlasmaCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/joelinrome/PlasmaCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package com.joelinrome;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PlasmaCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
