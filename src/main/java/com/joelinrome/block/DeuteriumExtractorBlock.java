204186e14c9ee9bc4fcd3dcb5e0c8489db80d9ef
JoelLucaAdams <joelucadams@gmail.com>
2024-09-01 20:56:30 +0100

    Delete redundant imports

--- src/main/java/com/joelinrome/block/DeuteriumExtractorBlock.java
+++ src/main/java/com/joelinrome/block/DeuteriumExtractorBlock.java
@@ -2,15 +2,9 @@ package com.joelinrome.block;
 
 import net.minecraft.block.*;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.DirectionProperty;
-import net.minecraft.state.property.Properties;
-import net.minecraft.text.Text;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.BlockRotation;
-import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
